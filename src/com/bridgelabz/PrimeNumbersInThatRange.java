079d78822c8b32f63c978bb499fbfb7758867d1b
Surabhi951 <surabhibhagat95@gmail.com>
2022-11-26 18:26:10 +0530

    Modified: Checked prime numbers between 0 to 1000

--- src/com/bridgelabz/PrimeNumbersInThatRange.java
+++ src/com/bridgelabz/PrimeNumbersInThatRange.java
@@ -2,24 +2,30 @@ package com.bridgelabz;
 
 public class PrimeNumbersInThatRange {
 
-    public static void main(String[] args) {
-        PrimeNumbersInThatRange numbers = new PrimeNumbersInThatRange();
-        for (int i = 2; i < 1000; i++) {
-            boolean isPrime = numbers.checkPrimeNumbers(i);
-            if (isPrime){
-                System.out.println(i + " ");
-            }
-        }
-    }
+        public void checkPrimeNumbers() {
+            int minBound = 0;
+            int maxBound = 1000;
+            for(int i = minBound + 2; i<maxBound; i++)
+            {
 
-    public boolean checkPrimeNumbers(int i) {
-        boolean isPrime = true;
-        for (int j = 2; j <= i/2; j++) {
-            if (i % j == 0) {
-                isPrime = false;
-                break;
+                int flag = 1;
+                for (int j = 2; j <= i/2; j++)
+                {
+                    if(i % j == 0)
+                    {
+                        flag = 0;
+                        break;
+                    }
+                }
+                if(flag == 1)
+                {
+                    System.out.println(i + " ");
+                }
             }
         }
-        return isPrime;
-    }
+
+        public static void main(String[] args) {
+            PrimeNumbersInThatRange numbers = new PrimeNumbersInThatRange();
+            numbers.checkPrimeNumbers();
+        }
 }
